95c525d53e74d71a7596570af64d520e613f2d05
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-11 20:08:11 -0300

    JAVASCRITP HTMO DOM

--- src/lerDadosHTML_Palavra.js
+++ src/lerDadosHTML_Palavra.js
@@ -1,8 +1,5 @@
 
-function lerDadosHTML_Palavra(){
     var textoUm = document.getElementById('primeiraPalavra');
     console.log("Estou na função" + textoUm);    
     console.log(textoUm);
     document.getElementById("textoUm_HTML").innerHTML = textoUm;
-}
-
