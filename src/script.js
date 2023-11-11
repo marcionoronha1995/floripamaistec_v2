f34b55a7d8284f629a68ea20037a3eb162aea8ea
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-11 16:40:25 -0300

    aula 3

---
+++ src/script.js
@@ -0,0 +1,8 @@
+var textoUm = document.getElementById('primeiraPalavra');
+function lerDadosHTML_Palavra(){
+    console.log("Estou na função");    
+    console.log(textoUm);
+    return textoUm;
+}
+console.log(textoUm)
+lerDadosHTML_Palavra();
