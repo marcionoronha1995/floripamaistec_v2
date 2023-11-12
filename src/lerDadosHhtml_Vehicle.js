5ccbc9e79feaaf2797a5550da79ad2a8971a4e20
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-12 05:29:59 -0300

    vehice javascript html

---
+++ src/lerDadosHhtml_Vehicle.js
@@ -0,0 +1,12 @@
+console.log("por dentro do checkbox...");
+const vehicle11 = document.getElementById("vehicle1").value;
+const vehicle12 = document.getElementById("vehicle2").value;
+const vehicle13 = document.getElementById("vehicle3").value;
+console.log(
+    vehicle11 +"..." +
+    vehicle12 +"..." +
+    vehicle13 +"..."
+);
+console.log(vehicle11);
+console.log(vehicle12);
+console.log(vehicle13);
