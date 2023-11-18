f984d67e806e435edd00c765cbd9c02d784d5a1c
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-18 04:30:22 -0300

    inner faltou o r

--- semanas/semana4/dom.js
+++ semanas/semana4/dom.js
@@ -1,12 +1,16 @@
 
 function mudartextoum(){
+    console.log("mudartextoum()");
     const textoum = document.getElementById("texto-1");
-    document.getElementById("texto-3").inneHTML = "este texto";
+    document.getElementById("texto-3").innerHTML = textoum.innerHTML;
     
 };
 
 function mudartextodois(){
     const textodois = document.getElementsByTagName("p");
+    const textotres = document.getElementById("texto-2");
     document.getElementById("texto-4").innerHTML = textodois[1].innerHTML;
+    document.getElementById("texto-5").innerHTML = textotres.innerHTML;
+    
     
 };
