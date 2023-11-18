a45f17c0af53c57e64f55cf2c1a9d3aa5632c799
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-18 10:10:36 -0300

    html css js dom

--- semanas/semana4/dom.js
+++ semanas/semana4/dom.js
@@ -3,7 +3,7 @@ function mudartextoum(){
     console.log("mudartextoum()");
     const textoum = document.getElementById("texto-1");
     document.getElementById("texto-3").innerHTML = textoum.innerHTML;
-    
+    document.getElementById("data-hora-1").innerHTML = "Data: " + Date();    
 };
 
 function mudartextodois(){
@@ -11,6 +11,8 @@ function mudartextodois(){
     const textotres = document.getElementById("texto-2");
     document.getElementById("texto-4").innerHTML = textodois[1].innerHTML;
     document.getElementById("texto-5").innerHTML = textotres.innerHTML;
-    
+    document.getElementById("data-hora-2").innerHTML = "Data: " + Date();   
     
 };
+
+s
