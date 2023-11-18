d4c11d7e52d911c3f4478e0b7212cdae3f92e39e
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-11-18 04:16:39 -0300

    getElement byId byClass

--- semanas/semana4/dom.js
+++ semanas/semana4/dom.js
@@ -1,7 +1,12 @@
-mudartextoum(){
-    console.log("dentra da funcao um mudar texto");
+
+function mudartextoum(){
+    const textoum = document.getElementById("texto-1");
+    document.getElementById("texto-3").inneHTML = "este texto";
+    
 };
 
-mudartextodois(){
-    console.log("dentro da funcao dois mudar texto");
+function mudartextodois(){
+    const textodois = document.getElementsByTagName("p");
+    document.getElementById("texto-4").innerHTML = textodois[1].innerHTML;
+    
 };
